33dbd878f2f46958b6606d29ff42185ff4914a15
Natthiya Chakaew <143895621+Natthiya663040114-3@users.noreply.github.com>
2024-11-08 00:28:54 +0700

    NEW

--- app/getUser/dropDown.tsx
+++ app/getUser/dropDown.tsx
@@ -5,30 +5,29 @@ export default function DropDown() {
         <div className="grid">
         <div>
             <select className="justify-center text-white bg-gradient-to-r from-blue to-indigo-900 font-medium rounded-full text-m px-28 py-4 text-center">
-                <option value="TEA" className="text-center text-black text-m justify-center">ชานมไข่มุก</option>
+                <option value="SODA" className="text-center text-black text-m justify-center">น้ำอัดลม</option>
                 <option value="COFFEE" className="text-center text-black text-m">กาแฟ</option>
-                <option value="THAITEA" className="text-center text-black text-m">ชาไทย</option>
-                <option value="MILK" className="text-center text-black text-m">นมสด</option>
-                <option value="YOGURT" className="text-center text-black text-m">โยเกิร์ต</option>
-                <option value="SODA" className="text-center text-black text-m">โซดา</option>
-                <option value="JUICE" className="text-center text-black text-m">น้ำผลไม้</option>
-                <option value="WATER" className="text-center text-black text-m">น้ำดื่ม</option>
-                <option value="BEER" className="text-center text-black text-m">เบียร์</option>
-                
+                <option value="TEA" className="text-center text-black text-m">ชา</option>
+                <option value="MILK" className="text-center text-black text-m">นม</option>
+                <option value="JUICE" className="text-center text-black text-m">น้ำผลไม้</option>    
             </select>  
         </div>
         <div>
             <select className="justify-center text-white bg-gradient-to-r from-blue to-indigo-900 font-medium rounded-full text-m px-28 py-4 text-center mt-4">  
-            <option value="TEA" className="text-center text-black text-m justify-center">ชานมไข่มุก</option>
+            <option value="SODA" className="text-center text-black text-m justify-center">น้ำอัดลม</option>
                 <option value="COFFEE" className="text-center text-black text-m">กาแฟ</option>
-                <option value="THAITEA" className="text-center text-black text-m">ชาไทย</option>
+                <option value="TEA" className="text-center text-black text-m">ชา</option>
+                <option value="MILK" className="text-center text-black text-m">นม</option>
+                <option value="JUICE" className="text-center text-black text-m">น้ำผลไม้</option>
             </select>  
         </div>
         <div>
             <select className="justify-center text-white bg-gradient-to-r from-blue to-indigo-900 font-medium rounded-full text-m px-28 py-4 text-center mt-4">
-            <option value="TEA" className="text-center text-black text-m justify-center">ชานมไข่มุก</option>
+            <option value="SODA" className="text-center text-black text-m justify-center">น้ำอัดลม</option>
                 <option value="COFFEE" className="text-center text-black text-m">กาแฟ</option>
-                <option value="THAITEA" className="text-center text-black text-m">ชาไทย</option>
+                <option value="TEA" className="text-center text-black text-m">ชา</option>
+                <option value="MILK" className="text-center text-black text-m">นม</option>
+                <option value="JUICE" className="text-center text-black text-m">น้ำผลไม้</option>
             </select>  
         </div>
         </div>
--- app/getUser/page.tsx
+++ app/getUser/page.tsx
@@ -59,9 +59,6 @@ const GetUser = () => {
   }
 
 
-
-
-
   return (
 
     <div className="relative">

002e145a65705576eed8a8e3adc03142ad2c2985
Yuttana-Hoomda <yhumda@gmail.com>
2024-11-07 14:26:21 +0700

    fix error

--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// import RegisterComponent from "./register";
-
-// export default function Register(){
-//     return(
-//         <RegisterComponent/>
-//     )
-// }

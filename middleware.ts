12d212a9fd0d314ac808eaeb8dae128a4d3605ad
Yuttana-Hoomda <yhumda@gmail.com>
2024-11-08 16:12:36 +0700

    fix error server

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+// export async function middleware(request: NextRequest) {
+//     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
     
-    if (!token) {
-        return NextResponse.redirect(new URL('/', request.url))
-    }
+//     if (!token) {
+//         return NextResponse.redirect(new URL('/', request.url))
+//     }
     
-    return NextResponse.next()
-}
+//     return NextResponse.next()
+// }
 
-export const config = {
-  matcher: ['/home', '/beverage/:path*', '/calendar', '/getUser']
-}
+// export const config = {
+//   matcher: ['/home', '/beverage/:path*', '/calendar', '/getUser']
+// }

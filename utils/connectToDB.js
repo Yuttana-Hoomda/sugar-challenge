7637e370a0e3bcbdb3b2fc6fdf9959d0c14e05ef
Yuttana-Hoomda <yhumda@gmail.com>
2024-11-13 16:51:58 +0700

    update connect to db

--- utils/connectToDB.js
+++ utils/connectToDB.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'SC-DB',
       serverSelectionTimeoutMS: 20000,
     });
 
